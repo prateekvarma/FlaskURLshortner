3fe5c0fba425643d8a66132d2bcbf23f30ecd55b
Prateek Varma <prtk.varma@gmail.com>
2020-11-30 17:50:34 -0600

    check for duplicate url-codes, and display flash message

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import json
+import os.path
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 
 @app.route('/')
 def home():
@@ -11,6 +13,18 @@ def home():
 def your_url():
     if request.method == 'POST':
         urls = {}
+        #read the json file(if exists)
+        if os.path.exists('urls.json'):
+            with open('urls.json') as url_file2:
+                #read contents of the json file
+                urls = json.load(url_file2)
+        #check if the url-code already exists. The keys() will check all key references        
+        if request.form['code'] in urls.keys():
+            #url-code alredy exists, so don't save, display flash
+            flash('Short code already used :( choose another ')
+            return redirect(url_for('home'))
+
+        #write url and  url-code into the json file
         urls[request.form['code']] = { 'url':  request.form['url']}
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
